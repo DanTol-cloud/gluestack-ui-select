bd7819743d34a292a6192bfc2c155c99eae13937
DanTol-cloud <danielkozac05@gmail.com>
2024-08-22 18:17:12 +0300

    test-app

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  NativeBaseProvider,
+  Center,
+  Box,
+  Select,
+  CheckIcon,
+} from "@gluestack-ui/themed-native-base";
 
 export default function App() {
+  const [service, setService] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center height="100%">
+        <Box
+          maxW="300"
+          flex="1"
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Select
+            selectedValue={service}
+            minWidth="200"
+            accessibilityLabel="Choose Service"
+            placeholder="Choose Service"
+            _selectedItem={{
+              bg: "teal.600",
+              endIcon: <CheckIcon size="5" />,
+            }}
+            mt={1}
+            onValueChange={(itemValue) => setService(itemValue)}
+          >
+            <Select.Item label="UX Research" value="ux" />
+            <Select.Item label="Web Development" value="web" />
+            <Select.Item label="Cross Platform Development" value="cross" />
+            <Select.Item label="UI Designing" value="ui" />
+            <Select.Item label="Backend Development" value="backend" />
+          </Select>
+        </Box>
+      </Center>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
